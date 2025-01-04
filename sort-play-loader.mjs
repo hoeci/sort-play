c2109bd08f142b0902e0c5ba1c61b0dc1ff1d1ab
hoeci <164579566+hoeci@users.noreply.github.com>
2025-01-04 12:00:43 +0330

    Delete sort-play-loader.mjs

--- sort-play-loader.mjs
+++
@@ -1,5 +0,0 @@
-fetch("https://raw.githubusercontent.com/hoeci/sort-play/main/sort-play.js")
-    .then(res => res.text()) 
-    .then(content => new Blob([content], { type: "application/javascript" }))
-    .then(URL.createObjectURL)
-    .then(url => import(url));
